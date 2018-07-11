d92b0eed601708ba43fbc7d2343e9e5762f0f54a
Chris Cardle <cdcardle@gmail.com>
2018-07-11 14:46:11 +0000

    automatically backed up by learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+one = Post.create(title: "First", content: "This is the first post.")
+two = Post.create(title: "Second", content: "Here is the second one.")
+three = Post.create(title: "Third", content: "Third time's the charm, maybe.")
+four = Post.create(title: "Fourth", content: "Is this the last post?")
+
+news = Category.create(name: "News")
+entertainment = Category.create(name: "Entertainment")
+music = Category.create(name: "Music")
+
+joe = User.create(username: "joe", email: "joe@yahoo.com")
+bob = User.create(username: "bob", email: "bob@yahoo.com")
+susan = User.create(username: "susan", email: "susan@yahoo.com")
+sam = User.create(username: "sam", email: "sam@yahoo.com")
+
+comment1 = Comment.create(content: "Cool")
+comment2 = Comment.create(content: "Awesome")
+comment3 = Comment.create(content: "Terrible")
+comment4 = Comment.create(content: "First!")
+comment5 = Comment.create(content: "You stink")
+comment6 = Comment.create(content: "I hate this post")
+comment7 = Comment.create(content: "I hate you")
+comment8 = Comment.create(content: "Wahhhhh")
+comment9 = Comment.create(content: "I am bob")
+
+one.comments << comment1
+one.comments << comment2
+one.comments << comment3
+two.comments << comment4
+two.comments << comment5
+three.comments << comment6
+three.comments << comment7
+three.comments << comment8
+four.comments << comment9
+
+joe.comments << comment1
+bob.comments << comment2
+susan.comments << comment3
+sam.comments << comment4
+joe.comments << comment5
+sam.comments << comment6
+susan.comments << comment7
+sam.comments << comment8
+bob.comments << comment9
